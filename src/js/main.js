45e125c96f6cc87b969eac0bf792b83d539947fd
qamorozov <andr.morozov96@gmail.com>
2022-05-07 19:12:05 +0300

    intro (desktop)

--- src/js/main.js
+++ src/js/main.js
@@ -9,12 +9,31 @@ document.querySelector('.header-search__button').addEventListener('click', (e) =
 
 const catalogDropDown = document.querySelector('.catalog-dropdown');
 
-document.querySelector('.menu__list-item-link--catalog').addEventListener('click', () => {
+document.querySelector('.menu__list-item-link--catalog').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.body.classList.toggle('shadow');
     catalogDropDown.classList.toggle('catalog-dropdown--active');
 });
 
 window.addEventListener('click', (e) => {
     if (!e.target.closest('.menu__list-item-link--catalog') && !e.target.closest('.catalog-dropdown')) {
+        document.body.classList.remove('shadow');
         catalogDropDown.classList.remove('catalog-dropdown--active');
     };
+});
+
+// === INTRO SLIDER ===
+
+let introSlider = new Swiper(".intro__slider", {
+    // slideClass: 'intro__slider-item',
+    // wrapperClass: 'intro__slider-wrapper',
+    effect: "fade",
+    slidesPerView: 'auto',
+    speed: 1000,
+    loop: true,
+    autoplay: true,
+    autoplay: {
+        delay: 7000,
+    },
+    grabCursor: true,
 });
